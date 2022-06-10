fe026d4e40655adcd42a0fe97bf703bbc319e498
Konstantin Romanenko <kromanenko@lenovo.com>
2022-06-10 19:02:24 +0300

    Add build script

--- webpack.prod.config.ts
+++ webpack.prod.config.ts
@@ -9,7 +9,8 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const config: Configuration = merge(commonConfig, {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js'
+    filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/'
   },
   plugins: [
     new WebpackContainer.ModuleFederationPlugin({
